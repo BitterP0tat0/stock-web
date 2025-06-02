2a372d56201f1a213ddf65246ef87d1161d6bdc0
dama123450 <a251080262@gmail.com>
2025-06-02 12:29:09 +0200

    add login page

--- scripts/Navigationbar/Navigation.js
+++ scripts/Navigationbar/Navigation.js
@@ -1,38 +1,22 @@
-
 ///////////////////////////////////////////////////////////////
 ///////////Dark Mode Button/////////////////////
 
-  const toggleBtn = document.getElementById("mode-toggle");
-  const body = document.body;
-  const nav = document.querySelector("nav.navigation");
-  const footer = document.querySelector("footer");
+const toggleBtn = document.getElementById("mode-toggle");
+const body = document.body;
 
-  toggleBtn.addEventListener("click", () => {
-    if (body.classList.contains("light-mode")) {
-      body.classList.remove("light-mode");
-      body.classList.add("dark-mode");
+toggleBtn.addEventListener("click", () => {
+  if (body.classList.contains("light-mode")) {
+    body.classList.remove("light-mode");
+    body.classList.add("dark-mode");
 
-      nav.classList.remove("light-mode");
-      nav.classList.add("dark-mode");
+    toggleBtn.textContent = "Light Mode";
+  } else {
+    body.classList.remove("dark-mode");
+    body.classList.add("light-mode");
 
-      footer.classList.remove("light-mode");
-      footer.classList.add("dark-mode");
-
-      toggleBtn.textContent = "Light Mode";
-    } else {
-      body.classList.remove("dark-mode");
-      body.classList.add("light-mode");
-
-      nav.classList.remove("dark-mode");
-      nav.classList.add("light-mode");
-
-      footer.classList.remove("dark-mode");
-      footer.classList.add("light-mode");
-
-      toggleBtn.textContent = "Dark Mode";
-    }
-  });
-
+    toggleBtn.textContent = "Dark Mode";
+  }
+});
 
 ////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////
